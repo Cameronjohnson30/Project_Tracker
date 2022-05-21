765d81ea8103f2bdca5e9b7dbf2f1aed28f9cd77
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-05-21 00:35:29 -0700

    add reguires, and db connection to project table

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ require('pg')
 require('project')
 require('volunteer')
 require('pry')
+
+DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM projects *;")
+
+  end
+end
